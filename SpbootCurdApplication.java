df944940f2b4351b3e43dd883ae9e538d3ba1c44
1A0502Ainduvathi <135848517+1A0502Ainduvathi@users.noreply.github.com>
2024-11-25 11:02:28 +0530

    Add files via upload

---
+++ SpbootCurdApplication.java
@@ -0,0 +1,13 @@
+package com.example.spboot_curd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootCurdApplication.class, args);
+	}
+
+}
